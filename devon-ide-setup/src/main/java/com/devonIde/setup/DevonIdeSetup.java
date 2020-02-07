d27047861794a295ca9035dd495bb7a7424c6b5e
Rohit Rai <41901970+Rohit90700@users.noreply.github.com>
2020-02-07 16:02:09 +0530

    changes slaces

--- devon-ide-setup/src/main/java/com/devonIde/setup/DevonIdeSetup.java
+++ devon-ide-setup/src/main/java/com/devonIde/setup/DevonIdeSetup.java
@@ -33,12 +33,14 @@ public class DevonIdeSetup {
     URLConnection urlConnection = url.openConnection();
     System.out.println("Connection open.........");
     BufferedInputStream in = new BufferedInputStream(urlConnection.getInputStream());
-    File destFile = new File(absPath + FILE_SEPARATOR + "devonfw-ide-scripts-3.2.2.tar.gz");
+    File destFile = new File(FILE_SEPARATOR + "SWTBOT-repo" + FILE_SEPARATOR + "download" + FILE_SEPARATOR
+        + "devonfw-ide-scripts-3.2.2.tar.gz");
     System.out.println("File create for devon ide" + destFile.getAbsolutePath());
-    // destFile.createNewFile();
+    destFile.createNewFile();
     System.out.println("File created");
 
-    FileOutputStream out = new FileOutputStream(absPath + FILE_SEPARATOR + "devonfw-ide-scripts-3.2.2.tar.gz");
+    FileOutputStream out = new FileOutputStream(FILE_SEPARATOR + "SWTBOT-repo" + FILE_SEPARATOR + "download"
+        + FILE_SEPARATOR + "devonfw-ide-scripts-3.2.2.tar.gz");
     System.out.println("Start downloading");
     int i = 0;
     byte[] bytesIn = new byte[3000000];
