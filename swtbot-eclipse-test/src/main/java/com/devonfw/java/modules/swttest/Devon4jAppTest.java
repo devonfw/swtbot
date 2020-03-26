e180d1e246aa948bc5af525b850279640e0e865a
Rohit Rai <41901970+Rohit90700@users.noreply.github.com>
2020-03-26 15:51:11 +0530

    jenkins changes

--- swtbot-eclipse-test/src/main/java/com/devonfw/java/modules/swttest/Devon4jAppTest.java
+++ swtbot-eclipse-test/src/main/java/com/devonfw/java/modules/swttest/Devon4jAppTest.java
@@ -49,22 +49,21 @@ public class Devon4jAppTest {
     bot.button("Refresh").click();
     bot.button("Finish").click();
     bot.toolbarButtonWithTooltip("&Restore").click();
-    bot.waitUntil(new AllJobsAreFinished(), 1000000);
+    bot.waitUntil(new AllJobsAreFinished(), 10000);
 
     runProjectTestCases();
 
   }
 
   private void runProjectTestCases() {
-
-    bot.sleep(1000000000);
+	  
     bot.tree().getTreeItem("devon4japp-core").select();
     bot.tree().getTreeItem("devon4japp-core").expand();
 
     bot.tree().getTreeItem("devon4japp-core").getNode("src/test/java").expand();
     bot.tree().getTreeItem("devon4japp-core").getNode("src/test/java").getNode("com.test.general.service.impl.rest")
         .expand();
-    bot.waitUntil(new AllJobsAreFinished(), 100000);
+    bot.waitUntil(new AllJobsAreFinished(), 10000);
     bot.tree().getTreeItem("devon4japp-core").getNode("src/test/java").getNode("com.test.general.service.impl.rest")
         .getNode("SecurityRestServiceImplTest.java").select().contextMenu("Run As").menu("1 JUnit Test").click();
     bot.waitUntil(new AllJobsAreFinished(), 10000);
